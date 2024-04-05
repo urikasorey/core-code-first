3ff53251301b4c58ac03d9d5c44a25b5abd982c6
urikasorey <nhanhoctot1234@gmail.com>
2024-04-05 14:46:07 +0700

    add courses controller
    add courses controller

---
+++ EF Core_Code_first/EF Core_Code_first/Controllers/CoursesController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EF_Core_Code_first.Models;
+using EF_Core_Code_first.Services;
+
+namespace EF_Core_Code_first.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CoursesController : ControllerBase
+	{
+		private readonly ISchoolService _coursesService;
+
+		public CoursesController(ISchoolService coursesService)
+		{
+			_coursesService = coursesService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetCourses()
+		{
+			var authors = await _coursesService.GetCoursesAsync();
+
+			if (authors == null)
+			{
+				return StatusCode(StatusCodes.Status204NoContent, "No authors in database");
+			}
+
+			return StatusCode(StatusCodes.Status200OK, authors);
+		}
+
+		[HttpGet("id")]
+		public async Task<IActionResult> GetCourses(Guid id, bool includeCourses = false)
+		{
+			Courses courses = await _coursesService.GetCoursesAsync(id, includeCourses);
+
+			if (courses == null)
+			{
+				return StatusCode(StatusCodes.Status204NoContent, $"No Author found for id: {id}");
+			}
+
+			return StatusCode(StatusCodes.Status200OK, courses);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Courses>> AddCourses(Courses courses)
+		{
+			var dbCourses = await _coursesService.AddCoursesAsync(courses);
+
+			if (dbCourses == null)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, $"{courses.CoursesName} could not be added.");
+			}
+
+			return CreatedAtAction("GetCourses", new { id = courses.CoursesId }, courses);
+		}
+
+		[HttpPut("id")]
+		public async Task<IActionResult> UpdateCourses(Guid id, Courses courses)
+		{
+			if (id != courses.CoursesId)
+			{
+				return BadRequest();
+			}
+
+			Courses dbCourses = await _coursesService.UpdateCoursesAsync(courses);
+
+			if (dbCourses == null)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, $"{courses.CoursesName} could not be updated");
+			}
+
+			return NoContent();
+		}
+
+		[HttpDelete("id")]
+		public async Task<IActionResult> DeleteCourses(Guid id)
+		{
+			var courses = await _coursesService.GetCoursesAsync(id, false);
+			(bool status, string message) = await _coursesService.DeleteCoursesAsync(courses);
+
+			if (status == false)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, message);
+			}
+
+			return StatusCode(StatusCodes.Status200OK, courses);
+		}
+	}
+}
---
+++ EF Core_Code_first/EF Core_Code_first/Services/ISchoolService.cs
@@ -0,0 +1,31 @@
+﻿using EF_Core_Code_first.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EF_Core_Code_first.Services
+{
+	public interface ISchoolService
+	{
+		// Students Services
+		Task<List<Students>> GetStudentssAsync(); // GET All Studentss
+		Task<Students> GetStudentsAsync(Guid id); // GET Single Students
+		Task<Students> AddStudentsAsync(Students Students); // POST New Students
+		Task<Students> UpdateStudentsAsync(Students Students); // PUT Students
+		Task<(bool, string)> DeleteStudentsAsync(Students Students); // DELETE Students
+
+		// Course Services
+		Task<IEnumerable<Courses>> GetCoursesAsync();// GET All Courses
+		Task<Courses> GetCoursesAsync(Guid id, bool includeCourses = false);// GET Single Course
+		Task<Courses> AddCoursesAsync(Courses courses);// POST New Course
+		Task<Courses> UpdateCoursesAsync(Courses courses); // PUT Course
+		Task<(bool, string)> DeleteCoursesAsync(Courses courses);// DELETE Course
+
+		// StudentsCourse Services
+		Task<List<StudentCourses>> GetStudentsCoursesAsync(); // GET All StudentsCourses
+		Task<StudentCourses> GetStudentsCourseAsync(Guid studentId, Guid courseId); // GET Single StudentCourse
+		Task<StudentCourses> AddStudentCourseAsync(StudentCourses studentCourse); // POST New StudentCourse
+		Task<StudentCourses> UpdateStudentCourseAsync(StudentCourses studentCourse); // PUT StudentCourse
+		Task<(bool, string)> DeleteStudentCourseAsync(StudentCourses studentCourse); // DELETE StudentCourse
+		Task<ActionResult<IEnumerable<Students>>> GetStudentsAsync();
+	}	
+}
